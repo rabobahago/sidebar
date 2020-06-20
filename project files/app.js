dcda36f03ccef1ce969d3e36d99fb6776a2852f3
rabobahago <rabobahago@gmail.com>
2020-06-20 18:50:49 +0100

    project final added

--- project files/app.js
+++ project files/app.js
@@ -3,9 +3,14 @@ const closeBtn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.sidebar');
  
 toggleBtn.addEventListener('click', ()=>{
-    if(sidebar.classList.contains('show-sidebar')){
-        sidebar.classList.remove('show-sidebar');
-    }else{
-        sidebar.classList.add('show-sidebar');
-    }
+    // if(sidebar.classList.contains('show-sidebar')){
+    //     sidebar.classList.remove('show-sidebar');
+    // }else{
+    //     sidebar.classList.add('show-sidebar');
+    // }
+    sidebar.classList.toggle('show-sidebar');
+})
+
+closeBtn.addEventListener('click', ()=>{
+    sidebar.classList.remove('show-sidebar');  
 })
